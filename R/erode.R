9988834492c4e4b93af2bf3cf418a3f165b1f0b3
Vincenzo Coia <vincenzo.coia@gmail.com>
2024-07-10 16:00:11 -0700

    fix erode() arg

--- R/erode.R
+++ R/erode.R
@@ -2,7 +2,7 @@
 #'
 #' While `gbem()` calculates fluvial geomorphic change, `erode()`
 #' executes the change to create a modified channel or cross section.
-#' @param gbem The output of `gbem()`, containing the channel change.
+#' @param object The output of `gbem()`, containing the channel change.
 #' @returns The original cross section supplied to gbem, with possibly
 #' modified width based on the gbem run.
 #' @export
